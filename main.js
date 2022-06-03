09c2c8f9ab16db5045e4515fc5cbde88b3a49212
Ahmed Obad <63117335+amo4oma@users.noreply.github.com>
2022-06-04 07:02:48 +0800

    adding files

---
+++ main.js
@@ -0,0 +1,30 @@
+gsap.config({nullTargetWarn: false});
+
+let tl = gsap.timeline({
+    scrollTrigger :{
+        trigger : "#home",
+        pin: '#home', 
+        triggerHook: 0,
+        scrub: 0.1,
+        start : "top top",
+        end: (window.innerHeight * 2),
+ 
+    }
+})
+
+
+tl.fromTo('.right-logo',5, {x:"0%"},{x:"140%"},"first")
+tl.fromTo('.left-logo',5, {x:"0%"},{x:"-100%"},"first")
+tl.from('.wlecom h1',5, {opacity:'0'},"second")
+tl.from('.wlecom p',5, {opacity:'0'},"third")
+tl.fromTo('.box',2, {width:'0%'},{width:'95%'},'forth')
+tl.to('.wlecom',5, { opacity:'0',display:"none"},"fif")
+tl.to('.second-message',5, {y:"30%",opacity:'1',display:"block"},"six")
+tl.to('.second-message',5, { opacity:'0',display:"none"},"sev")
+tl.to('.third-message',5, {y:"30%",opacity:'1',display:"block"},"egh")
+tl.to('.third-message',5, { opacity:'0',display:"none"},"nine")
+tl.to('.right-logo',5, {scale: 1.5, opacity:'0',display:"none"},"h")
+tl.to('.left-logo',5, {scale:1.5, opacity:'0',display:"none"},"h")
+
+
+ 
